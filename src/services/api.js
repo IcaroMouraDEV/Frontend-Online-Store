d31510d0c01e05f940d3586e01722b03fb75a8c6
Icaro Joel <icaromoura.programador@gmail.com>
2022-07-07 16:13:46 -0300

    req - part 2/2

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,12 @@ export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
 }
