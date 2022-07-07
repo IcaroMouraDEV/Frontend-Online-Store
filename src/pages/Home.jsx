072d6231a53ad80e55ab6253e4eeb896a39b8aff
Icaro Joel <icaromoura.programador@gmail.com>
2022-07-07 17:13:12 -0300

    req complete

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Main searched={ false } />
+    );
+  }
+}
+
+export default Home;
