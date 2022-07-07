40d4f18538d19852ef2c0497127091d38f8be9ae
Gabriel Garcia <philiasmaximus@gmail.com>
2022-07-07 19:12:46 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route />
+        <Route path="/carrinho" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// req3
+// req 3
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <Link data-testid="shopping-cart-button" to="/carrinho">Carrinho</Link>
+    );
+  }
+}
+
+export default Header;
