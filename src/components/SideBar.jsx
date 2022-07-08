ac5358064929bfd38807fc119a852b110001c7a0
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-07-08 11:01:48 -0300

    requisite-4 complete

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,1 +1,41 @@
-// req4
+import React from 'react';
+import { getCategories } from '../services/api';
+import SideItem from './SideBarItem';
+
+class SideBar extends React.Component {
+  constructor() {
+    super();
+    this.state = { categoriesList: [] };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const getCategoriesList = await getCategories();
+    this.setState({
+      categoriesList: getCategoriesList,
+    });
+    // return getCategoriesList;
+    // return console.log(getCategoriesList);
+  }
+
+  render() {
+    const { categoriesList } = this.state;
+    return (
+      <div>
+        {
+          categoriesList.map((category) => (
+            <SideItem
+              key={ category.id }
+              categoryName={ category.name }
+            />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default SideBar;
