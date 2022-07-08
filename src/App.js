03f7e68d90ce019f793286626933b76eca8e7f72
Icaro Joel <icaromoura.programador@gmail.com>
2022-07-08 16:05:39 -0300

    req 6 complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
 import Cart from './pages/Cart';
-import Header from './components/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route path="/carrinho" component={ Cart } />
